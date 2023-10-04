cae9f79d08683916ccaf5cd26636c53f55bf583b
Ondřej Čertík <ondrej@certik.us>
2023-10-04 13:58:37 +0200

    Build it

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 set -ex
+
+as exit.asm -o exit.o
+ld exit.o -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib/ -lSystem -o exit
 clang++ -W -Wall -Werror parse_macho.cpp && ./a.out
