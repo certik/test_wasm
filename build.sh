df3f90fdd09456e16c7c87053cc0a8a0c4cdcf5e
Ondřej Čertík <ondrej@certik.us>
2023-10-04 14:30:34 +0200

    Compile in debug mode

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -ex
 
 as exit.asm -o exit.o
 ld exit.o -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib/ -lSystem -o exit
-clang++ -W -Wall -Werror parse_macho.cpp && ./a.out
+clang++ -g -W -Wall -Werror parse_macho.cpp && ./a.out
