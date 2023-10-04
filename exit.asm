f660e236183b5bacc909e05dfc5247e97f5376c7
Ondřej Čertík <ondrej@certik.us>
2023-10-04 14:54:59 +0200

    Add some calculation

--- exit.asm
+++ exit.asm
@@ -2,6 +2,9 @@
 .global _main
 .align 4
 _main:
-        mov x0, #42     ; exit code
+        mov x1, #5
+        mov x2, #6
+        add x0, x1, x2
+        ;mov x0, #42     ; exit code
         mov x16, #1     ; syscall number for exit
         svc #0x80       ; do the syscall!
