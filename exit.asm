4efdee175301ef2b18f11d12bc70ab45f193a8ca
Ondřej Čertík <ondrej@certik.us>
2023-10-10 12:07:54 +0200

    Do a simpler test

--- exit.asm
+++ exit.asm
@@ -4,13 +4,7 @@
 _main:
         mov x1, #5
         mov x2, #6
-        add x0, x1, x2, lsl #2
-        sub x0, x1, x0, lsr #1
-        mul x0, x1, x0
-        madd x0, x1, x0, xzr
-        madd x0, x1, x0, x2
-        udiv x0, x1, x0
-        bl -0x18
-        mov x0, #42     ; exit code
+        add x0, x1, x2
+        ; mov x0, #42     ; exit code
         mov x16, #1     ; syscall number for exit
         svc #0x80       ; do the syscall!
