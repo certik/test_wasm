e7a95c986dd39a355525e1f37965235f23e1406d
Ondřej Čertík <ondrej@certik.us>
2023-10-15 14:53:03 +0200

    Add sections

--- write_macho.cpp
+++ write_macho.cpp
@@ -31,8 +31,8 @@ int main() {
             .cputype = CPU_TYPE_ARM64,
             .cpusubtype = 0,
             .filetype = 2,
-            .ncmds = 17,
-            .sizeofcmds = 1056,
+            .ncmds = 16,
+            .sizeofcmds = 744,
             .flags = 2097285,
             .reserved = 0,
         };
@@ -61,7 +61,7 @@ int main() {
         // LC_SEGMENT_64
         segment_command_64 segment = {
             .cmd = LC_SEGMENT_64,
-            .cmdsize = 392,
+            .cmdsize = 232,
             .segname = "__TEXT",
             .vmaddr = 0x100000000,
             .vmsize = 0x4000,
@@ -69,28 +69,46 @@ int main() {
             .filesize = 16384,
             .maxprot = 5,
             .initprot = 5,
-            .nsects = 4,
+            .nsects = 2,
             .flags = 0,
         };
         vec_append(data, (uint8_t*)&segment, sizeof(segment));
-    }
 
-    {
-        // LC_SEGMENT_64
-        segment_command_64 segment = {
-            .cmd = LC_SEGMENT_64,
-            .cmdsize = 152,
-            .segname = "__DATA_CONST",
-            .vmaddr = 0x100004000,
-            .vmsize = 0x4000,
-            .fileoff = 16384,
-            .filesize = 16384,
-            .maxprot = 3,
-            .initprot = 3,
-            .nsects = 1,
-            .flags = 16,
-        };
-        vec_append(data, (uint8_t*)&segment, sizeof(segment));
+        {
+            // Section 0
+            section_64 section = {
+                .sectname = "__text",
+                .segname = "__TEXT",
+                .addr = 0x100003fa0,
+                .size = 0x14,
+                .offset = 16288,
+                .align = 4,
+                .reloff = 0,
+                .nreloc = 0,
+                .flags = 2147484672,
+                .reserved1 = 0,
+                .reserved2 = 0,
+            };
+            vec_append(data, (uint8_t*)&section, sizeof(section));
+        }
+
+        {
+            // Section 1
+            section_64 section = {
+                .sectname = "__unwind_info",
+                .segname = "__TEXT",
+                .addr = 0x100003fb4,
+                .size = 0x48,
+                .offset = 16308,
+                .align = 2,
+                .reloff = 0,
+                .nreloc = 0,
+                .flags = 0,
+                .reserved1 = 0,
+                .reserved2 = 0,
+            };
+            vec_append(data, (uint8_t*)&section, sizeof(section));
+        }
     }
 
     {
@@ -99,10 +117,10 @@ int main() {
             .cmd = LC_SEGMENT_64,
             .cmdsize = 72,
             .segname = "__LINKEDIT",
-            .vmaddr = 0x100008000,
+            .vmaddr = 0x100004000,
             .vmsize = 0x4000,
-            .fileoff = 32768,
-            .filesize = 658,
+            .fileoff = 16384,
+            .filesize = 451,
             .maxprot = 1,
             .initprot = 1,
             .nsects = 0,
